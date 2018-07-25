d9f7205562ceacfbb75f894485d69359e9b42c6c
Shuanghao <li407@indiana.edu>
2018-07-25 16:28:38 -0700

    Made changes for production

--- client/src/store.js
+++ client/src/store.js
@@ -6,17 +6,22 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancer =
-  process.env.NODE_ENV !== "production" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        name: "App",
-        actionBlacklist: ["REDUX_STORAGE_SAVE"]
-      })
-    : compose;
+// const composeEnhancer =
+//   process.env.NODE_ENV !== "production" &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//         name: "App",
+//         actionBlacklist: ["REDUX_STORAGE_SAVE"]
+//       })
+//     : compose;
 
-const enhancer = composeEnhancer(applyMiddleware(...middleware));
+// const enhancer = composeEnhancer(applyMiddleware(...middleware));
 
-const store = createStore(rootReducer, initialState, enhancer);
+// const store = createStore(rootReducer, initialState, enhancer);
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
 
 export default store;
